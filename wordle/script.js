10eee114af0c4305a58f1cfa57a98051ae8bccf1
Justin Bottinga <77456234+Kxpler@users.noreply.github.com>
2024-01-09 14:25:45 +0100

    Update script.js

--- wordle/script.js
+++ wordle/script.js
@@ -10,16 +10,9 @@ var randWordUpper = randWordLower.toLocaleUpperCase();
 var answer = randWordUpper.split("");
 console.log(answer);
 
-//lock de line als die vol zit
-// (j -1) % 5? 
-// empty row als het ingevulde woord niet bestaat
-// als een woord 2 of meer letters heeft en het antwoord niet, 
-// maakt hij maar 1 van die letters geel en niet allemaal
+// count amount of occurrences of a string in an array
+const countOccurrences = (arr, val) => arr.reduce((a, v) => (v === val ? a + 1 : a), 0);
 
-document.getElementById("reload").style.backgroundColor = "rgba(117, 160, 68, 0.3)";
-document.getElementById("reload").onclick = function () {
-    document.querySelector("h3").textContent = "Woord nog niet geraden";
-}
 
 function allBoxes(d) {
     let key;
@@ -32,10 +25,11 @@ function allBoxes(d) {
     }
     key = (String.fromCharCode(keynum));
 
-    cells[j].textContent = myKeyPress(event);
-
-    j++;
-
+    if (keynum != 13) {
+        cells[j].textContent = myKeyPress(event);
+        j++;
+    }
+    
     // backspace 
     if (keynum == 8) {
         j--;
@@ -43,81 +37,69 @@ function allBoxes(d) {
         word = word.slice(0, -1);
         j = j - 1;
         wordString = wordString.slice(0, -1);
-    } else {
+    } else if (keynum != 13) {
         word.push(myKeyPress(event));
         wordString += key;
     }
 
-
     // check enough keys are pressed then evaluate the word
-    if (j % 5 == 0) {
+    //keynum 13 = 'enter'
+    if (keynum == 13) {
+        if (j % 5 == 0) {
+            console.log(wordString)
+            console.log(word);
+            if (fiveLetterWords.includes(wordString.toLocaleLowerCase())) {
+                for (let i = 0; i < 5; i++){
+                    if (word[i] == answer[i]) {
+                        cells[j - (5 - i)].classList.add("Correct");
+                        document.getElementById(`${word[i]}`).classList.add("Correct");
+                    } else if (answer.includes(word[i])) {
+                        cells[j - (5 - i)].classList.add("Contains");
+                        document.getElementById(`${word[i]}`).classList.add("Contains");
+                    }
+                    else {
+                        cells[j - (5 - i)].classList.add("Incorrect");
+                        document.getElementById(`${word[i]}`).classList.add("Incorrect");
+                    }
+                }
+                word = [];
+                wordString = "";
+                // check if they all share the 'correct' class 
+                /*
+                if (cells[j - 5].className == cells[j - 4].className && cells[j - 4].className == cells[j - 3].className
+                    && cells[j - 3].className == cells[j - 2].className && cells[j - 2].className == cells[j - 1].className && cells[j - 1].className == "cell Correct") {
 
-        console.log(wordString)
-        console.log(word);
-        if (fiveLetterWords.includes(wordString.toLocaleLowerCase())) {
-            if (word[0] == answer[0]) {
-                cells[j - 5].classList.add("Correct");
-            } else if (answer.includes(word[0])) {
-                cells[j - 5].classList.add("Contains");
-            }
-            else {
-                cells[j - 5].classList.add("Incorrect");
-            }
+                    document.querySelector("h3").textContent = "";
 
-            if (word[1] == answer[1]) {
-                cells[j - 4].classList.add("Correct");
-            } else if (answer.includes(word[1])) {
-                cells[j - 4].classList.add("Contains");
-            }
-            else {
-                cells[j - 4].classList.add("Incorrect");
-            }
+                    if (keynum == 13) {
+                        location.reload()
+                    }
+                }
+                */
+                let correctCells = 0;
+                for (let x = 5; x >= 1; x--) {
+                    if (cells[j - x].className == 'cell Correct') {
+                        correctCells++;
+                    }
+                }
 
-            if (word[2] == answer[2]) {
-                cells[j - 3].classList.add("Correct");
-            } else if (answer.includes(word[2])) {
-                cells[j - 3].classList.add("Contains");
-            }
-            else {
-                cells[j - 3].classList.add("Incorrect");
-            }
+                if (correctCells == 5) {
+                    document.querySelector("h3").textContent = "";
 
-            if (word[3] == answer[3]) {
-                cells[j - 2].classList.add("Correct");
-            } else if (answer.includes(word[3])) {
-                cells[j - 2].classList.add("Contains");
-            }
-            else {
-                cells[j - 2].classList.add("Incorrect");
-            }
-
-            if (word[4] == answer[4]) {
-                cells[j - 1].classList.add("Correct");
-            } else if (answer.includes(word[4])) {
-                cells[j - 1].classList.add("Contains");
-            }
-            else {
-                cells[j - 1].classList.add("Incorrect");
-            }
-            word = [];
-            wordString = "";
-
-
-        } else {
-            word = [];
-            wordString = "";
-            alert("Not a word");
-        }
-
-
-        // check if they all share the 'correct' class 
-        if (cells[j - 5].className == cells[j - 4].className && cells[j - 4].className == cells[j - 3].className
-            && cells[j - 3].className == cells[j - 2].className && cells[j - 2].className == cells[j - 1].className && cells[j - 1].className == "cell Correct") {
-
-            document.getElementById("reload").style.backgroundColor = "rgb(0, 255, 64)";
-            document.querySelector("h3").textContent = "";
-            document.getElementById("reload").onclick = function () {
-                location.reload();
+                    if (keynum == 13) {
+                        //location.reload()
+                    }
+                }
+            } else {
+            cells[j - 5].textContent = "";
+            cells[j - 4].textContent = "";
+            cells[j - 3].textContent = "";
+            cells[j - 2].textContent = "";
+            cells[j - 1].textContent = "";
+            j = j - 5;
+            word = word.slice(0, -5);
+            wordString = wordString.slice(0, -5)
+            setInterval(showPopup(), 5000);
             }
         }
     }
@@ -137,15 +119,20 @@ function myKeyPress(e) {
     console.log(key);
     console.log(keynum);
 
-
     if (bannedKeys.includes(keynum)) {
         try {
             key = null;
-        } catch {
-
+        } catch (err) {
+            console.error(err);
         }
     }
-
-
     return key.toLocaleUpperCase();
+}
+
+function showPopup() {
+    document.getElementById("popup").style.display = "flex";
+}
+
+function removePopup() {
+    document.getElementById("popup").style.display = "none";
 }
